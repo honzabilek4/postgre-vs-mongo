28f5035ecf6ded6b71e34c10ee42b8e268e29edd
Jan Bílek <honza@elision.design>
2019-03-26 17:30:35 +0100

    Update compose file and it's dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Test');
+const { Client } = require('pg');
+const connectionString = 'postgresql://root:root@pg:5432/test';
+
+// const client = new Client({    
+//         user: 'root',
+//         host: 'pg',
+//         database: 'test',
+//         password: 'root',
+//         port: 5432,
+// })
+// client.connect();
+
+// client.query('SELECT NOW()', (err, res) => {
+//     console.log(err, res)
+//     client.end()
+// })
